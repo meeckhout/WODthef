8fb5bf3944bb171703eddf156a404361cd0bd8d5
meeckhout <marthe.eeckhout@gmail.com>
2022-08-08 14:26:52 +0200

    Get project ready

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './components/Header.jsx';
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import headerImg from 'src/assets/images/headerImg.png';
+
+const Header = () => (
+    <div className="header">
+        <img src={headerImg} alt="crossfit" />
+    </div>
+)
+
+export default Header;
